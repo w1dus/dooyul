38f48369cbbcc7f8c36450a84f83bce718dd3495
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2026-01-06 18:03:55 +0900

    퍼블 진행중

--- script/script.js
+++ script/script.js
@@ -3,8 +3,19 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
   right_slide_menu();
+  insights_tab();
 
 })
+
+const insights_tab = () => {
+  $('.main .insights-section .tab-list .item').click(function(){
+    const index = $(this).index();
+    $('.main .insights-section .tab-list .item').removeClass('active');
+    $(this).addClass('active');
+    $('.main .insights-section .content-list > li').removeClass('show')
+    $('.main .insights-section .content-list > li').eq(index).addClass('show')
+  })
+}
 
 
 const right_slide_menu = () => {
