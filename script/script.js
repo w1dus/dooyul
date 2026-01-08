b9af3def8cffb8bdd7c3f7893fa0e971773bfa91
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2026-01-08 19:03:01 +0900

    퍼블 진행중

--- script/script.js
+++ script/script.js
@@ -4,13 +4,27 @@ document.addEventListener("DOMContentLoaded", function(e){
 
   right_slide_menu();
   insights_tab();
-
+  member_popup();
+  
 })
 
+const member_popup = () => {
+  for (let i = 1; i <= 4; i++) {
+    $('#popup' + i + '-btn').click(function(e) {
+        $('#popup' + i).addClass('show');
+        e.preventDefault();
+    });
+    
+    $('#popup' + i + ' .close-btn').click(function() {
+        $('#popup' + i).removeClass('show');
+    });
+  }
+}
+
 const insights_tab = () => {
-  $('.main .insights-section .tab-list .item').click(function(){
+  $('.main .insights-section .tab-list button.item').click(function(){
     const index = $(this).index();
-    $('.main .insights-section .tab-list .item').removeClass('active');
+    $('.main .insights-section .tab-list button.item').removeClass('active');
     $(this).addClass('active');
     $('.main .insights-section .content-list > li').removeClass('show')
     $('.main .insights-section .content-list > li').eq(index).addClass('show')
